8556598025832040203d674eac28169a0b4bd91b
MakyKari <maki.kari@inbox.ru>
2024-02-24 18:18:52 +0600

    added home page display products

---
+++ controllers/homecontroller/homecontroller.go
@@ -0,0 +1,37 @@
+package homecontroller
+
+import (
+	"fmt"
+	"go-web-native/entities"
+	"go-web-native/models/productmodel"
+	"net/http"
+	"text/template"
+)
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	products := productmodel.Getall()
+
+	sortedProducts := make(map[entities.Category][]entities.Product)
+	for _, product := range products {
+		categoryName := product.Category
+		sortedProducts[categoryName] = append(sortedProducts[categoryName], product)
+	}
+
+	data := map[string]interface{}{
+        "SortedProducts": sortedProducts,
+    }
+
+	fmt.Println(sortedProducts)
+
+	tmpl, err := template.ParseFiles("views/home/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
