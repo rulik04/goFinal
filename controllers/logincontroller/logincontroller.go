abe85654e2adac4e3764df8eb544d480701f33bc
rulik04 <220625@astanait.edu.kz>
2024-03-06 17:50:04 +0500

    +session

--- controllers/logincontroller/logincontroller.go
+++ controllers/logincontroller/logincontroller.go
@@ -5,9 +5,11 @@ import (
 	"go-web-native/models/loginmodel"
 	"net/http"
 	"text/template"
+
+	"github.com/gorilla/sessions"
 )
 
-func Verify(w http.ResponseWriter, r *http.Request) {
+func Verify(w http.ResponseWriter, r *http.Request, store *sessions.CookieStore) {
 	if r.Method == "GET" {
 		temp, err := template.ParseFiles("views/login/index.html")
 		if err != nil {
@@ -19,17 +21,23 @@ func Verify(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "POST" {
 		var user entities.User
+		session, err := store.Get(r, "secret")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
 		user.Name = r.FormValue("name")
 		user.Email = r.FormValue("email")
 		user.Password = r.FormValue("password")
-
 		ok := loginmodel.Verify(user)
 		if !ok {
 			temp, _ := template.ParseFiles("views/login/index.html")
 			temp.Execute(w, nil)
 		}
-
+		session.Values["authenticated"] = true
+		session.Save(r, w)
+	
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
--- main.go
+++ main.go
@@ -9,35 +9,72 @@ import (
 	"go-web-native/controllers/registercontroller"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/sessions"
 )
 
-func main() {
-	// 	Database connection
-	config.ConnectDB()
+var store = sessions.NewCookieStore([]byte("rulan-aisultan-golang-final-project"))
 
+func main() {
+	// Database connection
+	config.ConnectDB()
 	// Routes
-	// 1.Homepage
-	http.HandleFunc("/", homecontroller.Welcome)
+	// 1. Homepage
+	http.HandleFunc("/", CheckSession(homecontroller.Welcome))
 
 	// 2. Category
-	http.HandleFunc("/categories", categorycontroller.Index)
-	http.HandleFunc("/categories/add", categorycontroller.Add)
-	http.HandleFunc("/categories/edit", categorycontroller.Edit)
-	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+	http.HandleFunc("/categories", CheckSession(categorycontroller.Index))
+	http.HandleFunc("/categories/add", CheckSession(categorycontroller.Add))
+	http.HandleFunc("/categories/edit", CheckSession(categorycontroller.Edit))
+	http.HandleFunc("/categories/delete", CheckSession(categorycontroller.Delete))
 
 	// 3. Products
-	http.HandleFunc("/products", productcontroller.Index)
-	http.HandleFunc("/products/add", productcontroller.Add)
-	http.HandleFunc("/products/detail", productcontroller.Detail)
-	http.HandleFunc("/products/edit", productcontroller.Edit)
-	http.HandleFunc("/products/delete", productcontroller.Delete)
+	http.HandleFunc("/products", CheckSession(productcontroller.Index))
+	http.HandleFunc("/products/add", CheckSession(productcontroller.Add))
+	http.HandleFunc("/products/detail", CheckSession(productcontroller.Detail))
+	http.HandleFunc("/products/edit", CheckSession(productcontroller.Edit))
+	http.HandleFunc("/products/delete", CheckSession(productcontroller.Delete))
 
 	// 4. Login
-	http.HandleFunc("/login", logincontroller.Verify)
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+        logincontroller.Verify(w, r, store)
+    })
+	http.HandleFunc("/logout", logoutHandler)
 
 	// 5. Register
 	http.HandleFunc("/register", registercontroller.Create)
+
 	// Run server
 	log.Println("Server running on port: 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func CheckSession(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, err := store.Get(r, "secret")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
+
+		handler(w, r)
+	}
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "secret")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	session.Values["authenticated"] = false
+	session.Save(r, w)
+
+	http.Redirect(w, r, "/login", http.StatusFound)
 }
